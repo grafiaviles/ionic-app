508f111622d2c265ae5c686dadfb3b6d46c78cf3
grafiaviles <grafiaviles@gmail.com>
2016-12-26 19:09:58 -0300

    Cambio de servicios

--- www/js/services.js
+++ www/js/services.js
@@ -6,30 +6,17 @@ angular.module('starter.services', [])
   // Some fake testing data
   this.data = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    desc: 'Ben Sparrow',
+    completado: false,
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    desc: 'Max Lynx',
+    completado: false,
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    desc: 'Adam Bradleyson',
+    completado: false,
+  }]
 
     this.all = function all () {
         return this.data;
